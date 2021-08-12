87ed92c1df0e106c3f72538339022326f31d4d94
SergioGalvan <sergio.galvan@2bcore.com>
2021-08-12 08:51:36 -0500

    mensaje

--- index.js
+++ index.js
@@ -145,7 +145,7 @@ server.post("/curso", (req, res) => {
             resultado = LibreriaSGG.respuestaBasica(textoEnviar);
         } else if (!memoria) {
             textoEnviar = "Es necesario elegir el tamaño de la memoria:";
-            opciones = ["8-gb", "16-gb"];
+            opciones = ["8-gb", "16-gb", "4-gb","32-gb"];
             resultado = LibreriaSGG.respuestaBasica(textoEnviar);
         } else if (!discoduro) {
             textoEnviar = "Ahora veremos el almacenamiento en disco:";
